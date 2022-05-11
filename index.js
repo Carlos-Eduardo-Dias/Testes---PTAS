6a605c2a6a37e85a948e42b9bb9bf093ac6f33c7
Carlos Dias <86132535+Carlos-Eduardo-Dias@users.noreply.github.com>
2022-05-11 21:28:16 +0000

    Rotas para empresas

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
 var express = require("express");
 var app = express();
-var { usuario } = require("./models");
+var { usuario, empresa } = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
-}))
+}));
 
-app.get("/", async function(req, res){
+app.get("/usuarios", async function(req, res){
   var resultado = await usuario.findAll();
   res.json(resultado);
 });
 
-app.get("/:id", async function(req, res){
+app.get("/usuarios/:id", async function(req, res){
   var resultado = await usuario.findOne({where: {id: req.params.id}});
   res.json(resultado);
 });
 
-app.post("/", async function(req, res){
+app.post("/usuarios", async function(req, res){
   var resultado = await usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", async function(req, res){
+app.put("/usuarios/:id", async function(req, res){
   var resultado = await usuario.update(req.body,{where: {id: req.params.id}});
   res.json(resultado);
 });
 
-app.delete("/:id", async function(req, res){
+app.delete("/usuarios/:id", async function(req, res){
   var resultado = await usuario.destroy({where: {id: req.params.id}});
   res.json(resultado);
 });
+ // rotas para empresas
 
-app.listen(3000, function(){
-  console.log("O servidor ta funfando");
+app.get("/empresas", async function(req, res){
+  var resultado = await empresa.findAll();
+  res.json(resultado);
+});
+
+app.get("/empresas/:id", async function(req, res){
+  var resultado = await empresa.findOne({where: {id: req.params.id}});
+  res.json(resultado);
+});
+
+app.post("/empresas", async function(req, res){
+  var resultado = await empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/empresas/:id", async function(req, res){
+  var resultado = await empresa.update(req.body,{where: {id: req.params.id}});
+  res.json(resultado);
+});
+
+app.delete("/empresas/:id", async function(req, res){
+  var resultado = await empresa.destroy({where: {id: req.params.id}});
+  res.json(resultado);
+});
+
+app.listen(3100, function(){
+  console.log("O servidor ta 10 de Outubro");
 });
